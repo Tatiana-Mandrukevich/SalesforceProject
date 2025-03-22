aeede93177bbf2871db684043ab7fab07e17a7c3
Tatiana Mandrukevich <tatiana.mandrukevich@icloud.com>
2025-03-22 23:49:35 +0300

    bugs fixed

--- src/main/java/waiters/Waiter.java
+++ src/main/java/waiters/Waiter.java
@@ -8,7 +8,6 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
-import java.util.List;
 
 public class Waiter {
 
@@ -20,25 +19,6 @@ public class Waiter {
             boolean noAjaxRequest = (Boolean) js.executeScript("return (typeof jQuery != 'undefined') ? jQuery.active == 0 : true");
             return documentReady && noAjaxRequest;
         });
-    }
-
-    public static void waitForDropdownToBeClickable(WebDriver driver, String xpath, String label) {
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        WebElement element = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath(String.format(xpath, label))));
-        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView({behavior: 'smooth', block: 'center'});", element);
-        wait.until(ExpectedConditions.visibilityOf(element));
-        wait.until(ExpectedConditions.elementToBeClickable(element));
-    }
-
-    public static void waitForOptionToBeVisible(WebDriver driver, String xpath, String option) {
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(String.format(xpath, option))));
-    }
-
-    public static void waitForOptionToBeClickable(WebDriver driver, String xpath, String option) {
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
-        WebElement element = driver.findElement(By.xpath(String.format(xpath, option)));
-        wait.until(ExpectedConditions.elementToBeClickable(element));
     }
 
     public static void waitForButtonToBeClickable(WebDriver driver, WebElement button) {
